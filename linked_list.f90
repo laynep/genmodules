3237eec0b9201ab0a228018c3cbedd2bdbc8b41b
lpri691 <lpri691@aucklanduni.ac.nz>
2012-09-14 09:16:38 +1200

    created type(linkedlist)

--- linked_list.f90
+++ linked_list.f90
@@ -31,30 +31,17 @@ implicit none
 
 contains
 
-	!Initialize the list.  Remember that "head" and "tail" are bookend pointers for 
-	!the list, which point to the first and last nodes, respectively.
-	subroutine ll_init(head,tail)
-	implicit none
-
-		type(llnode), pointer, intent(inout) :: head, tail
-
-		!Makes an empty list, i.e. the node is null().
-		head => null()
-		tail => null()
-
-	end subroutine ll_init
-
 	!Subroutine which returns the nth node in list, where head is counted as the zeroth
 	!node.  Can optionally return the previous and next node, too.  If n=1, then 
 	!previous node is head and if n=n_end, then next node is null() for both next and
 	!prev.  If list is empty, then previous is head, next is tail, and sel has null() 		!pointers.  If n is out-of-bounds, then returns tail for all pointers.
 	!NOTE: this is much slower than the random access given by a simple array --- 
 	!O(n) vs O(1)
-	subroutine ll_nav(n,head,tail,sel,plus,minus)
+	subroutine ll_nav(n,list,sel,plus,minus)
 	implicit none
 
 		integer, intent(in) :: n
-		type(llnode), pointer, intent(inout) :: head,tail
+		type(linkedlist), intent(in) :: list
 		type(llnode), pointer, intent(out) :: sel
 		type(llnode), pointer, optional, intent(out) :: plus, minus
 		type(llnode), pointer :: move
@@ -67,30 +54,30 @@ contains
 
 		!Check to see if n is too small.
 		if (n==0) then
-			if (present(plus)) plus=>head
+			if (present(plus)) plus=>list%head
 			return
 		else if (n<0) then
 			return
 		end if
 
 		!Check if list is empty.
-		if (.not. associated(head)) then !List is empty.
+		if (.not. associated(list%head)) then !List is empty.
 			sel%next=>null()
-			if (present(minus)) minus=>head
-			if (present(plus)) plus=>tail
+			if (present(minus)) minus=>list%head
+			if (present(plus)) plus=>list%tail
 		else
 			!See if n=1.
 			if (n==1) then
-				sel=>head
-				if(present(plus)) plus=>head%next
+				sel=>list%head
+				if(present(plus)) plus=>list%head%next
 				return
 			end if
 
 			!Navigate down list.
-			move => head
+			move => list%head
 			do i=1, n
 				if (i==1) then
-					move => head
+					move => list%head
 				else
 					!Point to the next node in list.
 					move => move%next
@@ -99,7 +86,7 @@ contains
 				if (.not. associated(move)) then !we're at the end.
 					!Out of bounds.
 					if(i==n) then
-						if (present(minus)) minus=>tail
+						if (present(minus)) minus=>list%tail
 					end if
 					return
 				end if		
@@ -119,53 +106,54 @@ contains
 
 	!Add a node to the end of the list.  Takes as input a pointer which points to the
 	!node which we want to add.  
-	subroutine ll_append(new, head, tail)
+	subroutine ll_append(new, list)
 	implicit none
 
-		type(llnode), pointer, intent(inout) :: head, tail
+		type(linkedlist), intent(inout) :: list
 		type(llnode), pointer, intent(inout) :: new
 
 		!Check if the list is empty.
-		if (associated(head)) then !Not empty.
-			tail%next => new	!Whichever node is tail, connect to new.
-						!This should point to an unnamed llnode.
-						!i.e. new=>unnamed node defined via allocate.
-			new%next => null()	!
-			tail => new	
-					
-
+		if (associated(list%head)) then !Not empty.
+			!Whichever node is tail, connect to new.
+			!This should point to an unnamed llnode.
+			!i.e. new=>unnamed node defined via allocate.
+			list%tail%next => new	
+			new%next => null()	
+			list%tail => new	
 		else
-			head => new		!Attach new node to end of head, making it
-						!the first node.
-			tail => new		!Attach the tail, indicated new is the end.
-			tail%next => null()	!No successor to node that new points to.
+			!Attach new node to end of head, making it the first node.
+			list%head => new		
+			!Attach the tail, indicated new is the end.
+			list%tail => new	
+			!No successor to node that new points to.	
+			list%tail%next => null()	
 		end if
 
 	end subroutine ll_append
 
 	!Add a node to the end of the nth node in a list, making the new node the (n+1)st node
-	subroutine ll_insert(n,new,head, tail)
+	subroutine ll_insert(n,new,list)
 	implicit none
 
-		type(llnode), pointer, intent(inout) :: head, tail
+		type(linkedlist), intent(inout) :: list
 		type(llnode), pointer, intent(inout) :: new
 		type(llnode), pointer :: sel, plus
 		integer, intent(in) :: n
 
 		if(n==0) then
-			call ll_nav(n,head,tail,sel,plus)
-			head=>new
+			call ll_nav(n,list,sel,plus)
+			list%head=>new
 			new%next=>plus
 			return
 		end if
 	
 		!Navigate to nth node in list.
-		call ll_nav(n,head,tail,sel,plus)
+		call ll_nav(n,list,sel,plus)
 
 		!Check if n is more than numb nodes in list.
 		if (.not. associated(sel%next)) then !n or less nodes in list.
 			!Stick new on the end of list.
-			call ll_append(new,head,tail)
+			call ll_append(new,list)
 			return
 		else
 			new%next => plus
@@ -176,18 +164,18 @@ contains
 
 
 	!Print a list.
-	subroutine ll_print(head)
+	subroutine ll_print(list)
 	implicit none
 
-		type(llnode), pointer, intent(in) :: head
+		type(linkedlist), intent(in) :: list
 		type(llnode), pointer :: move
 		integer :: i
 
 		!Check if list is empty.
-		if (.not. associated(head)) then
+		if (.not. associated(list%head)) then
 			print*, "The list is empty."
 		else
-			move=>head
+			move=>list%head
 			do
 				print*,(move%a(i),i=1,size(move%a))
 				if (.not. associated(move%next)) then
@@ -201,10 +189,10 @@ contains
 	end subroutine ll_print
 
 	!Write a list to file, "fname".  Optional input are formt, and unit.
-	subroutine ll_write(head,fname,frmt,unumb)
+	subroutine ll_write(list,fname,frmt,unumb)
 	implicit none
 
-		type(llnode), pointer, intent(in) :: head
+		type(linkedlist), intent(in) :: list
 		character(len=*), intent(in) :: fname
 		character(len=*), optional, intent(in) :: frmt
 		integer, optional, intent(in) :: unumb
@@ -218,16 +206,16 @@ contains
 			numb = 31415927
 		end if
 		if (present(frmt)) then
-			open(unit=numb,file=fname,status="old",form=frmt)
+			open(unit=numb,file=fname,form=frmt)
 		else
-			open(unit=numb,file=fname,status="old",form="unformatted")
+			open(unit=numb,file=fname,form="unformatted")
 		end if
 
 		!Check if list is empty.
-		if (.not. associated(head)) then
+		if (.not. associated(list%head)) then
 			print*, "The list is empty."
 		else
-			move=>head
+			move=>list%head
 			do
 				write(unit=numb),(move%a(i),i=1,size(move%a))
 				move=>move%next
@@ -265,22 +253,22 @@ contains
 
 	!Change a list to an array that it is conformable with.  Assumes that all the 
 	!vectors in the list are of the same dimension.
-	subroutine ll_to_array(head, table)
+	subroutine ll_to_array(list, table)
 	implicit none
 
-		type(llnode), pointer, intent(inout) :: head
+		type(linkedlist), intent(inout) :: list
 		double precision, dimension(:,:), allocatable, intent(out) :: table
 		type(llnode), pointer :: move
 		integer :: counter, i
 
 		!Check if empty.
-		if (.not. associated(head)) then
+		if (.not. associated(list%head)) then
 			return
 		else
 			!Count number of elements in array st can allocate array.
 			!O(n)
 			counter=1
-			move=>head
+			move=>list%head
 			do
 				if (.not. associated(move%next)) then
 					exit
@@ -290,10 +278,10 @@ contains
 				end if
 			end do
 			!Allocate table
-			allocate(table(counter,size(head%a)))
+			allocate(table(counter,size(list%head%a)))
 
 			!Load the table with the list.
-			move=>head
+			move=>list%head
 			do i=1, counter
 				table(i,:)=move%a
 				if (associated(move%next)) move=>move%next
@@ -304,24 +292,24 @@ contains
 
 
 	!Delete the first element in a linked list.  Returns a pointer to the first node.
-	subroutine ll_del_first(head, tail, first)
+	subroutine ll_del_first(list, first)
 	implicit none
 
-		type(llnode), pointer, intent(inout) :: head, tail
+		type(linkedlist), intent(inout) :: list
 		type(llnode), pointer :: test
 		type(llnode), pointer, intent(out) :: first
 
 		!Check to see if list is empty.
-		if (associated(head)) then !list not empty.
+		if (associated(list%head)) then !list not empty.
 			!Check if more than 1 node.
-			if (associated(head%next)) then !more than one node.
-				first => head
+			if (associated(list%head%next)) then !more than one node.
+				first => list%head
 				!Makes sure unnamed array deallocated in mem.
-				head => head%next
+				list%head => list%head%next
 			else
-				first => head
-				head => null()
-				tail => null()
+				first => list%head
+				list%head => null()
+				list%tail => null()
 			end if
 			
 		else
@@ -333,19 +321,19 @@ contains
 
 
 	!Delete all elements in a list.  Leaves the list initialized.
-	subroutine ll_del_all(head,tail)
+	subroutine ll_del_all(list)
 	implicit none
 
-		type(llnode), pointer, intent(inout) :: head, tail
-		type(llnode), pointer :: next
-		
+		type(linkedlist), intent(inout) :: list
+		type(llnode), pointer :: move
+	
 		do
 			!Check if list empty.
-			if (.not. associated(head)) then
+			if (.not. associated(list%head)) then
 				exit
 			else
-				call ll_del_first(head,tail,next)
-				nullify(next)
+				call ll_del_first(list,move)
+				nullify(move)
 			end if			
 		end do
 
